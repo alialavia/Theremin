328b1020907792cd06b7992eb3f79f0f18589d14
atheler <atheler@student.ethz.ch>
2015-05-26 23:18:54 +0200

    Fixed pentatonic scale

--- musicscale.py
+++ musicscale.py
@@ -10,7 +10,7 @@ _major_map     = []
 _minor_pattern = {1, 4, 6, 9, 11}
 _minor_map     = []
 
-_pentatonic_pattern  = {1, 3, 8}
+_pentatonic_pattern  = {1, 3, 5, 8, 10}
 _pentatonic_pattern2 = {6, 11}
 _pentatonic_map      = []
 
@@ -43,8 +43,10 @@ for i in range(maxD):
 def freq_2_midi(f):
     return int(69 + 12 * log2(f / a440))
 
+
 def midi_2_freq(d):
     return power(2., (d - 69) / 12.) * a440
+
 
 def _scaler(mapping):
     def inner(freq):
@@ -53,10 +55,12 @@ def _scaler(mapping):
         return midi_2_freq(d_scaled)
     return inner
 
+
 scale_to_chroma     = _scaler(_chroma_map)
 scale_to_major      = _scaler(_major_map)
 scale_to_minor      = _scaler(_minor_map)
 scale_to_pentatonic = _scaler(_pentatonic_map)
+
 
 def no_scaling(f):
     return f

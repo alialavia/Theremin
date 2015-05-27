ce517bc56b196b8e6ed21680bf5b8f143495eff3
atheler <atheler@student.ethz.ch>
2015-05-27 21:38:08 +0200

    Simplification, clean up, __repr__(), pep8

--- soundgenerators.py
+++ soundgenerators.py
@@ -1,212 +1,246 @@
-'''Sound Makers'''
+'''Sound Generators'''
 import warnings
 import numpy as np
-tau = 2 * np.pi
 import scipy.signal
 import config
+tau = 2 * np.pi
+
 
 '''Waveform Functions'''
-sin      = np.sin
-square   = scipy.signal.square
+sin = np.sin
 sawtooth = scipy.signal.sawtooth
+square = scipy.signal.square
+
+
 def triangle(*args, **kwargs):
     return scipy.signal.sawtooth(*args, width=0.5, **kwargs)
 
 
 '''Sound Generators'''
+
+
 class Osc(object):
     '''Simple Oscillator
-    
-    Properties:
-        - frequency:    Oscillation Frequency
-        - waveformFunc: Function for waveform. sin, triangle, square, sawtooth
-    '''
-    def __init__(self, frequency=220, waveformFunc=sin):
-        self.frequency = frequency
-        self.waveformFunc = waveformFunc
-        self.__phase = 0
 
+    Attributes/Properies:
+        - frequency:     Oscillation frequency
+        - waveform_func: Function for signal generation
+    '''
+    def __init__(self, frequency=220, waveform_func=sin):
+        self.frequency = frequency
+        self.waveform_func = waveform_func
+        self._phase = 0.
 
     def read(self):
         tEnd = config.chunkSize / float(config.fs)
         ts = np.arange(0, tEnd, 1. / config.fs)
-        signal = self.waveformFunc(tau * self.frequency * ts + self.__phase)
-        self.__phase = (tau * self.frequency * tEnd + self.__phase) % tau
+        signal = self.waveform_func(tau * self.frequency * ts + self._phase)
+        self._phase = (tau * self.frequency * tEnd + self._phase) % tau
         return signal
+
+    def __repr__(self):
+        txt = 'Osc(frequency: {:.2f} Hz, waveform_func: {})'
+        return txt.format(self.frequency,
+                          self.waveform_func.__name__)
 
 
 class FMOsc(Osc):
     '''Frequency Modulated (FM) Oscillator
-    
-    Properties:
-        - frequency:    Oscillation Frequency
-        - waveformFunc: Function for waveform. sin, triangle, square, sawtooth
-        - fmFrequency:  Frequency of the Frequency Modulation (FM)
-        - fmAmount:     Intensity of the Frequency Modulation (FM)
-    '''
-    def __init__(self, frequency=220, waveformFunc=sin, fmFrequency=5, fmAmount=5):
-        super(FMOsc, self).__init__(frequency, waveformFunc)
-        self.fmAmount = fmAmount
-        self.__lfo = Osc(fmFrequency)
 
+    Attributes/Properies:
+        - frequency:     Oscillation frequency
+        - waveform_func: Function for signal generation
+        - fmFrequency:   Frequency of the frequency modulation (FM)
+        - fmAmount:      Intensity of the frequency modulation (FM)
+    '''
+    def __init__(self, frequency=220, waveform_func=sin, fmFrequency=5,
+                 fmAmount=10):
+        super(FMOsc, self).__init__(frequency, waveform_func)
+        self.fmAmount = fmAmount
+        self._lfo = Osc(fmFrequency)
 
     def read(self):
         tEnd = config.chunkSize / float(config.fs)
         ts = np.arange(0, tEnd, 1. / config.fs)
-        freqs = self.frequency + self.fmAmount * self.__lfo.read()
-        signal = self.waveformFunc(tau * freqs * ts + self._Osc__phase)
-        self._Osc__phase = (tau * freqs[-1] * tEnd + self._Osc__phase) % tau
+        freqs = self.frequency + self.fmAmount * self._lfo.read()
+        signal = self.waveform_func(tau * freqs * ts + self._phase)
+        self._phase = (tau * freqs[-1] * tEnd + self._phase) % tau
         return signal
-
 
     @property
     def fmFrequency(self):
-        return self._FMOsc__lfo.frequency
+        return self._lfo.frequency
 
     @fmFrequency.setter
     def fmFrequency(self, freq):
-        self._FMOsc__lfo.frequency = freq
+        self._lfo.frequency = freq
+
+    def __repr__(self):
+        txt = ('FMOsc(frequency: {:.2f} Hz, '
+               'waveform_func: {}, '
+               'fmFrequency: {:.2f} Hz, '
+               'fmAmount: {:.2f} Hz)')
+
+        return txt.format(self.frequency,
+                          self.waveform_func.__name__,
+                          self._lfo.frequency,
+                          self.fmAmount)
 
 
 class Theremin(object):
-    '''Virtual Theremin. Mixes sounds from two oscillators and uses a third for
-    Amplitude Modulation.
+    '''Theremin
 
-    Properties:
-        - frequency:        Oscillation Frequency
-        - waveformFunc1:    Waveform function for the first Oscillator (osc1).
-                            E.g. sin, triangle, square, sawtooth
-        - waveformFunc2:    Waveform function for the second Oscillator (osc2).
-                            E.g. sin, triangle, square, sawtooth
-        - vibratoFrequency: Frequency of the Vibrato Effect.
-        - vibratoAmount:    Intensity of the Vibrato Effect.
-        - tremoloFrequency: Frequency of Tremolo Effect
-        - tremoloAmount:    Intensity of Tremolo Effect. (values in [0.0, 1.0])
-        - volume:           Volume of sound. (values in [0.0, 1.0])
-        - morph:            Mix paramter of osc1 and osc2. (values in [0.0, 1.0])
+    One signal oscillators (FMOsc) and one lfo-oscillators for Amplitude
+    Modulation.
+
+    Attributes/Properies:
+        - frequency:        Signal oscillators frequency
+        - volume:           Volume of sound (values in [0.0, 1.0])
+        - waveform_func:    Function for signal generation (sin, triangle,
+                            square, sawtooth)
+        - vibratoFrequency: Frequency of the vibrato effect
+        - vibratoAmount:    Intensity of the vibrato effect
+        - tremoloFrequency: Frequency of tremolo effect
+        - tremoloAmount:    Intensity of tremolo effect (values in [0.0, 1.0])
     '''
-    def __init__(self, frequency=220, waveformFunc1=sin, waveformFunc2=square,
-            vibratoFrequency=5, vibratoAmount=0, tremoloFrequency=8,
-            tremoloAmount=0):
-        self.__osc1 = FMOsc(frequency, waveformFunc1, vibratoFrequency, vibratoAmount)
-        self.__osc2 = FMOsc(frequency, waveformFunc2, vibratoFrequency, vibratoAmount)
-        self.__lfo = Osc(tremoloFrequency)
-        self.__tremoloAmount = tremoloAmount
-        self.__volume = 1.0
-        self.__morph = 0.0
-
+    def __init__(self, frequency=220, waveform_func=sin, vibratoFrequency=5,
+                 vibratoAmount=0, tremoloFrequency=8, tremoloAmount=0):
+        self._fmOsc = FMOsc(frequency, waveform_func, vibratoFrequency,
+                            vibratoAmount)
+        self._lfo = Osc(tremoloFrequency)
+        self._tremoloAmount = tremoloAmount
+        self._volume = 1.0
 
     def read(self):
-        osc1Signal = self.__osc1.read()
-        osc2Signal = self.__osc2.read()
-        signal = (1.0 - self._Theremin__morph) * osc1Signal + self._Theremin__morph * osc2Signal
-        lfoSignal = self.__lfo.read()
-        return self.__volume * signal * (1. + .5 * self.__tremoloAmount * (lfoSignal - 1))
+        fmOscSignal = self._fmOsc.read()
+        lfoSignal = self._lfo.read()
+        signal = fmOscSignal * (1. + .5 * self._tremoloAmount * (lfoSignal - 1.))
+        return self._volume * signal
 
-
-    '''frequency'''
+    '''Frequency'''
     @property
     def frequency(self):
-        return self._Theremin__osc1.frequency
+        return self._fmOsc.frequency
 
     @frequency.setter
     def frequency(self, freq):
-        self._Theremin__osc1.frequency = freq
-        self._Theremin__osc2.frequency = freq
+        self._fmOsc.frequency = freq
 
-
-    '''waveform'''
-    @property
-    def waveformFunc1(self):
-        return self._Theremin__osc1.waveformFunc
-
-    @waveformFunc1.setter
-    def waveformFunc1(self, func):
-        self._Theremin__osc1.waveformFunc = func
-
-    @property
-    def waveformFunc(self):
-        return self._Theremin__osc1.waveformFunc
-
-    @waveformFunc.setter
-    def waveformFunc(self, func):
-        self._Theremin__osc1.waveformFunc = func
-
-    @property
-    def waveformFunc2(self):
-        return self._Theremin__osc2.waveformFunc
-
-    @waveformFunc2.setter
-    def waveformFunc1(self, func):
-        self._Theremin__osc2.waveformFunc = func
-
-
-    '''vibratoFrequency'''
-    @property
-    def vibratoFrequency(self):
-        return self._Theremin__osc1.fmFrequency
-
-    @vibratoFrequency.setter
-    def vibratoFrequency(self, freq):
-        self._Theremin__osc1.fmFrequency = freq
-        self._Theremin__osc2.fmFrequency = freq
-
-
-    '''vibratoAmount'''
-    @property
-    def vibratoAmount(self):
-        return self._Theremin__osc1.fmAmount
-
-    @vibratoAmount.setter
-    def vibratoAmount(self, amount):
-        self._Theremin__osc1.fmAmount = amount
-        self._Theremin__osc2.fmAmount = amount
-
-
-    '''tremoloFrequency'''
-    @property
-    def tremoloFrequency(self):
-        return self._Theremin__lfo.frequency
-
-    @tremoloFrequency.setter
-    def tremoloFrequency(self, freq):
-        self._Theremin__lfo.frequency = freq
-
-
-    '''tremoloAmount'''
-    @property
-    def tremoloAmount(self):
-        return self._Theremin__tremoloAmount
-
-    @tremoloAmount.setter
-    def tremoloAmount(self, val):
-        if 0.0 <= val <= 1.0:
-            self._Theremin__tremoloAmount = val
-        else:
-            warnings.warn('No legit value for tremoloAmount')
-
-
-    '''volume'''
+    '''Volume'''
     @property
     def volume(self):
-        return self._Theremin__volume
+        return self._volume
 
     @volume.setter
     def volume(self, val):
-        if 0.0 <= val <= 1.0:
-            self._Theremin__volume = val
-        else:
-            warnings.warn('No legit value for volume')
+        if val < 0 or 1 < val:
+            warnings.warn('%d No legit value for volume' % val)
+        self._volume = max(0., min(1., val))
 
-
-    '''morph'''
+    '''Waveform'''
     @property
-    def morph(self):
-        return self._Theremin__morph
+    def waveform_func(self):
+        return self._fmOsc.waveform_func
 
-    @morph.setter
-    def morph(self, val):
-        if 0.0 <= val <= 1.0:
-            self._Theremin__morph = val
-        else:
-            warnings.warn('No legit value for morph')
+    @waveform_func.setter
+    def waveform_func(self, func):
+        self._fmOsc.waveform_func = func
+
+    '''Vibrato Frequency'''
+    @property
+    def vibratoFrequency(self):
+        return self._fmOsc.fmFrequency
+
+    @vibratoFrequency.setter
+    def vibratoFrequency(self, freq):
+        self._fmOsc.fmFrequency = freq
+
+    '''Vibrato Amount'''
+    @property
+    def vibratoAmount(self):
+        return self._fmOsc.fmAmount
+
+    @vibratoAmount.setter
+    def vibratoAmount(self, amount):
+        self._fmOsc.fmAmount = amount
+
+    '''Tremolo Frequency'''
+    @property
+    def tremoloFrequency(self):
+        return self._lfo.frequency
+
+    @tremoloFrequency.setter
+    def tremoloFrequency(self, freq):
+        self._lfo.frequency = freq
+
+    '''Tremolo Amount'''
+    @property
+    def tremoloAmount(self):
+        return self._tremoloAmount
+
+    @tremoloAmount.setter
+    def tremoloAmount(self, val):
+        if val < 0 or 1 < val:
+            warnings.warn('%d No legit value for tremoloAmount' % val)
+        self._tremoloAmount = max(0., min(1., val))
+
+    def __repr__(self):
+        txt = ('Theremin(frequency: {:.2f} Hz, '
+               'volume: {:.1f}, '
+               'waveform_func: {}, '
+               'vibratoFrequency: {:.2f} Hz, '
+               'vibratoAmount: {:.1f}, '
+               'tremoloFrequency: {:.2f} Hz, '
+               'tremoloAmount: {:.1f})')
+
+        return txt.format(self.frequency,
+                          self.volume,
+                          self.waveform_func.__name__,
+                          self.vibratoFrequency,
+                          self.vibratoAmount,
+                          self.tremoloFrequency,
+                          self.tremoloAmount)
+
+
+'''Testing Properies'''
+if __name__ == '__main__':
+    # Testing Osc
+    osc = Osc()
+    osc.frequency = 1
+    osc.frequency
+    osc.waveform_func = triangle
+    osc.waveform_func
+    print osc
+    osc.read()
+
+    # Testing FMOsc
+    fmOsc = FMOsc()
+    fmOsc.frequency = 1
+    fmOsc.frequency
+    fmOsc.waveform_func = triangle
+    fmOsc.waveform_func
+    fmOsc.fmFrequency = 2
+    fmOsc.fmFrequency
+    fmOsc.fmAmount = 3
+    fmOsc.fmAmount
+    print fmOsc
+    fmOsc.read()
+
+    # Testing Theremin
+    theremin = Theremin()
+    theremin.frequency = 1
+    theremin.frequency
+    theremin.volume = 0.3
+    theremin.volume
+    theremin.waveform_func = triangle
+    theremin.waveform_func
+    theremin.vibratoFrequency = 2
+    theremin.vibratoFrequency
+    theremin.vibratoAmount = 3
+    theremin.vibratoAmount
+    theremin.tremoloFrequency = 4
+    theremin.tremoloFrequency
+    theremin.tremoloAmount = 0.7
+    theremin.tremoloAmount
+    print theremin
+    theremin.read()

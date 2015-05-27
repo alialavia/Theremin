17b5d24fd96f430e9593834a4f4128599e6c5b7c
atheler <atheler@student.ethz.ch>
2015-05-27 16:33:20 +0200

    Cleanup

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-fs          = 44100 # Sampling Rate
-nChannels   = 1     # Number of Audio channels
-sampleWidth = 2     # Number of Bytes per sample
-chunkSize   = 256   # Chunk Size
-baseNote    = 220   # baseNote Note frequency
-
-fmin = 261.626
-fmax = 2 * fmin
+fs          = 44100    # Sampling Rate
+nChannels   = 1        # Number of Audio channels
+sampleWidth = 2        # Number of Bytes per sample
+chunkSize   = 256      # Chunk Size
+fmin        = 261.626  # Minimum frequency of Virtual Theremin [Hz]
+fmax        = 2 * fmin # Maximum frequency of Virtual Theremin [Hz].
+                       # 2 * fmin -> One Octave

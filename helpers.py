71fb8e30d8fdc5e339ff8ad2f2ed1fd3f6e24118
atheler <atheler@student.ethz.ch>
2015-05-28 00:13:51 +0200

    Cleanup

--- helpers.py
+++ helpers.py
@@ -1,10 +1,8 @@
 import sys, os, inspect
 import struct
-from time import time
-from settings import *
+import config
 
-_maxVal = 2 ** (sampleWidth * 8) / 2.1
-_notelist = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']
+_notelist = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 def addLeapPath():
     src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
@@ -12,49 +10,19 @@ def addLeapPath():
     sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 def cutoff(value, minCut, maxCut):
-    if value > maxCut:
-        return maxCut
-    if value < minCut:
-        return minCut
-    return value
+    '''value -> range [minCut, maxCut]'''
+    return max(minCut, min(maxCut, value))
+
+_maxVal = 2 ** (config.sampleWidth * 8) / 2.1
 
 def convert(seq):
+    # Needed in __main__.py
     '''Converts numpy float array ([-1.0, 1.0]) to binary data (little endian)'''
     seq2 = _maxVal * seq
     fmt = '<' + len(seq) * 'h'
     return struct.pack(fmt, *seq2)
 
-
-_t0 = time()
-def ascending(m=5.0):
-    '''Ascending float value'''
-    return (time() - _t0) / m
-
-
-def descending(m=5.0):
-    '''Descending float value'''
-    return 1.0 - (time() - _t0) / m
-
-
 def print_on_same_line(fVal):
     '''Put Cursor back one line and print'''
     sys.stdout.write('\r%.4f' % fVal)
     sys.stdout.flush()
-
-
-def distance_to_frequency(d):
-    ''' Calculates frequency of a note d half steps away from A4 (440 Hz) '''
-    return 440.0 * pow(2.0, d / 12.0)
-
-
-def note_to_frequency(note, octave):    
-    ''' Calculates frequency of note in octave '''
-    if note in _notelist:
-        # Calculate distance from A4
-        d = (_notelist.index(note) - 9) + (12 * (octave - 4))
-        return distance_to_frequency(d)
-    else:
-        raise ValueError('Note should be one of these string values:' + ', '.join(_notelist))
-
-
-            
